88af0500ecb00d382b1f397efa30966ea15e72ee
ShravyaRNadig <shravyyarnadig@gmail.com>
2022-06-07 10:51:15 +0530

    footer compo added

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import Home from "./pages/Home.js";
 import Cuisine from "./pages/Cuisine.js";
 import ContactUs from "./pages/ContactUs";
 import Auth from './pages/Auth';
-
+import Footer from "./components/Footer";
 
 function App() {
   return (
 
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/cuisine" element={<Cuisine />}></Route>
-        <Route path="/contactus" element={<ContactUs />}></Route>
-      </Routes>
-    </BrowserRouter>
-
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/cuisine" element={<Cuisine />}></Route>
+          <Route path="/contactus" element={<ContactUs />}></Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 
