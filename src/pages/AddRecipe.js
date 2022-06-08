8424c8b44f0e63a3e98aca21630b8343d19afa33
vikas-nlb <vikas.nlb@gmail.com>
2022-06-08 16:46:33 +0530

    Add recipe is working

--- src/pages/AddRecipe.js
+++ src/pages/AddRecipe.js
@@ -1,9 +1,124 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import { useRecipe } from "../RecipeContext"
 
 const AddRecipe = () => {
-    return (
-        <h1>Add Recipe</h1>
-    )
+    const { foodRecipes, alterRecipeList, alterContributionList } = useRecipe();
+
+    const [food, setFood] = useState(foodRecipes);
+
+    const [countryList, setCountryList] = useState([])
+    const [selectedCountry, setSelectedCountry] = useState('default')
+
+    const [mealList, setMealList] = useState([])
+    const [selectedMeal, setSelectedMeal] = useState('default')
+
+    const [dishName, setDishName] = useState('')
+
+    const [ingredient, setIngredients] = useState('')
+
+    const [waysToPrepare, setWayToPrepare] = useState('')
+
+    useEffect(() => {
+        setFood(foodRecipes)
+    }, [foodRecipes])
+
+    useEffect(() => {
+        food.recipes.map((item) => {
+            setCountryList(countryList => [...countryList, item.country])
+            item.data.meals.map((mealItem) => {
+                setMealList(mealList => [...mealList, mealItem.name])
+            })
+        })
+        setMealList(mealList => [...new Set(mealList)])
+        setCountryList(countryList => [...new Set(countryList)])
+    }, [])
+
+
+    const onSelectedCountryValue = (event) => {
+        setSelectedCountry(event.target.value)
+    }
+    const onSelectedMealValue = (event) => {
+        setSelectedMeal(event.target.value)
+    }
+
+    const onDishNameChange = (event) => {
+        setDishName(event.target.value)
+    }
+
+    const onIngredientChange = (event) => {
+        setIngredients(event.target.value)
+    }
+
+    const onWayToPrepareChange = (event) => {
+        setWayToPrepare(event.target.value)
+    }
+    const onAddRecipe = () => {
+        alert('entered')
+        let temp = { ...food }
+        const ingredientsArr = ingredient.split(',')
+        const waysToPrepareArr = waysToPrepare.split(',')
+        const obj = {
+            "name": dishName,
+            "items": [...ingredientsArr],
+            "waysToPrepare": [...waysToPrepareArr],
+            "comments": [{ "name": "Author", "comment": "Unique dish and hope everyone loves it", "rating": 5 }]
+        }
+        console.log('Object',obj)
+        temp.recipes.map((countryItem) => {
+            if (countryItem.country === selectedCountry) {
+                console.log('Entered countryItem', countryItem)
+                countryItem.data.meals.map((mealItem) => {
+                    console.log('Entered mealItem', mealItem.name)
+                    if (mealItem.name === selectedMeal) {
+                        console.log('Entered mealItem', mealItem.name)
+                        mealItem.data.push(obj)
+                        alterRecipeList(temp)
+                        alert('Recipe successfully added')
+                    }
+                })
+            }
+        })
+    }
+
+    const handleAuthRedirect = () => {
+        window.location.href = '/'
+    }
+
+    if (sessionStorage.getItem("auth") === 'Authenticated') {
+        return (
+            <div>
+                <select onChange={onSelectedCountryValue} value={selectedCountry}>
+                    <option value="default" disabled>Select Country</option>
+                    {
+                        countryList.map((countryItem) =>
+                            (<option value={countryItem}>{countryItem}</option>)
+                        )
+                    }
+                </select>
+
+                <select onChange={onSelectedMealValue} value={selectedMeal}>
+                    <option value="default" disabled>Select Meal</option>
+                    {
+                        mealList.map((mealItem) =>
+                            (<option value={mealItem}>{mealItem}</option>)
+                        )
+                    }
+                </select>
+                <input value={dishName} onChange={onDishNameChange} placeholder='Name' />
+                <textarea value={ingredient} onChange={onIngredientChange} placeholder='Ingredients (Separate the items with a comma' />
+                <textarea value={waysToPrepare} onChange={onWayToPrepareChange} placeholder='List the ways to prepare (Separate the methods with a comma' />
+                <button onClick={onAddRecipe}>Add Recipe</button>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <h1>You are not authorized to view this page</h1>
+                <h2>Please login to view this page</h2>
+                <button onClick={handleAuthRedirect}>Login/Signup</button>
+            </div>
+        )
+    }
 
 }
 export default AddRecipe
