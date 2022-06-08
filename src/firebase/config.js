c046ed5b37d96581735360d1fb2c4de1a4e52cf6
ShravyaRNadig <shravyyarnadig@gmail.com>
2022-06-08 17:02:08 +0530

    Changing firebase congif

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCPCNW1oDBUTzeaYcK8NFJ8ghkv2HVf6e4",
-    authDomain: "recipe-ac95e.firebaseapp.com",
-    projectId: "recipe-ac95e",
-    storageBucket: "recipe-ac95e.appspot.com",
-    messagingSenderId: "1032953355430",
-    appId: "1:1032953355430:web:29e78f0bf74ff6d1506867",
-    measurementId: "G-ZF17QW5NQ3"
+  apiKey: "AIzaSyDM659nGrqIk9IQnMJcxeJB-xhj_39lWF8",
+  authDomain: "react-recipe-3c83c.firebaseapp.com",
+  databaseURL: "https://react-recipe-3c83c-default-rtdb.firebaseio.com",
+  projectId: "react-recipe-3c83c",
+  storageBucket: "react-recipe-3c83c.appspot.com",
+  messagingSenderId: "461326021945",
+  appId: "1:461326021945:web:da55aef8fd45841a47e4e8"
   };
   
   // Initialize Firebase
